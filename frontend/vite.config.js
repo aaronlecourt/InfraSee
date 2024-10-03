8c1150c0a66be79f5043785cfc26e444daff6e21
LesterOrtiz <20213282@s.ubaguio.edu>
2024-10-04 01:48:59 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
+  },
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'), // Adjust this according to your folder structure
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
