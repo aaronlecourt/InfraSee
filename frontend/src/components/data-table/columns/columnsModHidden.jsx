748477b3b251b28ff56d8eba9ef9a3c9f9777612
Aaron Lecourt <20217375@s.ubaguio.edu>
2024-10-25 17:04:14 +0800

    hidden tab multiselect

--- frontend/src/components/data-table/columns/columnsModHidden.jsx
+++ frontend/src/components/data-table/columns/columnsModHidden.jsx
@@ -11,6 +11,30 @@ const formatDate = (dateString) => {
 
 export const columnsModHidden = [
   {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+        className="translate-y-0.5"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+        className="translate-y-0.5"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
     accessorKey: "report_by",
     title: "Reported By",
     header: ({ column }) => (
