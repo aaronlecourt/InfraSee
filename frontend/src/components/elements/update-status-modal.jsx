d3a757b5bd1992767df2d659bd55e94396df995c
Aaron Lecourt <97894956+aaronlecourt@users.noreply.github.com>
2024-12-12 11:32:06 +0800

    ok na ata value

--- frontend/src/components/elements/update-status-modal.jsx
+++ frontend/src/components/elements/update-status-modal.jsx
@@ -343,7 +343,7 @@ export function UpdateStatusDialog({ isOpen, onClose, data }) {
                                     className="mt-1"
                                     rows="3"
                                     maxLength={150}
-                                    value={field.value || previousRemarks}
+                                    value={previousTimeResolved ? field.value : ""}
                                     onChange={(e) => {
                                       setRemarksLength(e.target.value.length);
                                       field.onChange(e);
